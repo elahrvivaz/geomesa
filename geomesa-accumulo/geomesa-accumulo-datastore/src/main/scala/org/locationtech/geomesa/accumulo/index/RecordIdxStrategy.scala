de17c806a6d4817285d67503891da8e46c8e4a95
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2016-06-07 13:44:16 -0400

    sfcurve updates         adding sys prop for range estimates         ranges methods         updating z range calls         breadth-first z ranges, handling or'd geoms in z3, need to do z2         wip working - need to fix env of geom collections         wip updating stratgies         wip or splitting as single query plan GEOMESA-347,GEOMESA-1215 GEOMESA-1168?

--- geomesa-accumulo/geomesa-accumulo-datastore/src/main/scala/org/locationtech/geomesa/accumulo/index/RecordIdxStrategy.scala
+++ geomesa-accumulo/geomesa-accumulo-datastore/src/main/scala/org/locationtech/geomesa/accumulo/index/RecordIdxStrategy.scala
@@ -20,7 +20,7 @@ import org.locationtech.geomesa.accumulo.iterators.{BinAggregatingIterator, Kryo
 import org.locationtech.geomesa.filter._
 import org.locationtech.geomesa.utils.geotools.RichSimpleFeatureType.RichSimpleFeatureType
 import org.opengis.feature.simple.SimpleFeatureType
-import org.opengis.filter.{Filter, Id}
+import org.opengis.filter.{And, Filter, Id, Or}
 
 import scala.collection.JavaConversions._
 
@@ -32,22 +32,21 @@ object RecordIdxStrategy extends StrategyProvider {
                                         filter: QueryFilter,
                                         transform: Option[SimpleFeatureType],
                                         stats: GeoMesaStats): Option[Long] = {
-    if (QueryFilterSplitter.isFullTableScan(filter)) Some(Long.MaxValue) else Some(1L)
+    if (filter.primary.isDefined) Some(1L) else Some(Long.MaxValue)
   }
 
   // top-priority index - always 1
   override protected def indexBasedCost(sft: SimpleFeatureType,
                                         filter: QueryFilter,
                                         transform: Option[SimpleFeatureType]): Long =
-    if (QueryFilterSplitter.isFullTableScan(filter)) Int.MaxValue else 1
+  if (filter.primary.isDefined) 1 else Long.MaxValue
 
-  def intersectIdFilters(filters: Seq[Filter]): Set[String] = {
-    if (filters.length < 2) {
-      filters.map(_.asInstanceOf[Id]).flatMap(_.getIDs.map(_.toString)).toSet
-    } else {
-      // get the Set of IDs in *each* filter and convert to a Scala immutable Set
-      // take the intersection of all sets, since the filters and joined with 'and'
-      filters.map(_.asInstanceOf[Id].getIDs.map(_.toString).toSet).reduceLeft(_ intersect _)
+  def intersectIdFilters(filter: Filter): Set[String] = {
+    filter match {
+      case f: And => f.getChildren.map(intersectIdFilters).reduceLeftOption(_ intersect _).getOrElse(Set.empty)
+      case f: Or  => f.getChildren.flatMap(intersectIdFilters).toSet
+      case f: Id  => f.getIDs.map(_.toString).toSet
+      case _ => throw new IllegalArgumentException(s"Expected ID filter, got ${filterToString(filter)}")
     }
   }
 }
@@ -60,57 +59,57 @@ class RecordIdxStrategy(val filter: QueryFilter) extends Strategy with LazyLoggi
     val featureEncoding = queryPlanner.ds.getFeatureEncoding(sft)
     val prefix = sft.getTableSharingPrefix
 
-    val ranges = if (filter.primary.forall(_ == Filter.INCLUDE)) {
-      // allow for full table scans - we use the record index for queries that can't be satisfied elsewhere
-      filter.secondary.foreach { f =>
-        logger.warn(s"Running full table scan for schema ${sft.getTypeName} with filter ${filterToString(f)}")
-      }
-      val start = new Text(prefix)
-      Seq(new aRange(start, true, aRange.followingPrefix(start), false))
-    } else {
-      // Multiple sets of IDs in a ID Filter are ORs. ANDs of these call for the intersection to be taken.
-      // intersect together all groups of ID Filters, producing Some[Id] if the intersection returns something
-      val identifiers = RecordIdxStrategy.intersectIdFilters(filter.primary)
-      output(s"Extracted ID filter: ${identifiers.mkString(", ")}")
-      if (identifiers.nonEmpty) {
+    val ranges = filter.primary match {
+      case None =>
+        // allow for full table scans
+        filter.secondary.foreach { f =>
+          logger.warn(s"Running full table scan for schema ${sft.getTypeName} with filter ${filterToString(f)}")
+        }
+        val start = new Text(prefix)
+        Seq(new aRange(start, true, aRange.followingPrefix(start), false))
+
+      case Some(primary) =>
+        // Multiple sets of IDs in a ID Filter are ORs. ANDs of these call for the intersection to be taken.
+        // intersect together all groups of ID Filters, producing a set of IDs
+        val identifiers = RecordIdxStrategy.intersectIdFilters(primary)
+        output(s"Extracted ID filter: ${identifiers.mkString(", ")}")
         identifiers.toSeq.map(id => aRange.exact(RecordTable.getRowKey(prefix, id)))
-      } else {
-        // TODO GEOMESA-347 instead pass empty query plan
-        Seq.empty
-      }
     }
 
-    val table = ds.getTableName(sft.getTypeName, RecordTable)
-    val threads = ds.getSuggestedThreads(sft.getTypeName, RecordTable)
+    if (ranges.isEmpty) { EmptyPlan(filter) } else {
+      val table = ds.getTableName(sft.getTypeName, RecordTable)
+      val threads = ds.getSuggestedThreads(sft.getTypeName, RecordTable)
+      val dupes = false // record table never has duplicate entries
 
-    if (sft.getSchemaVersion > 5) {
-      // optimized path when we know we're using kryo serialization
-      if (hints.isBinQuery) {
-        // use the server side aggregation
-        val iters = Seq(BinAggregatingIterator.configureDynamic(sft, filter.secondary, hints, deduplicate = false))
-        val kvsToFeatures = BinAggregatingIterator.kvsToFeatures()
-        BatchScanPlan(filter, table, ranges, iters, Seq.empty, kvsToFeatures, threads, hasDuplicates = false)
-      } else if (hints.isStatsIteratorQuery) {
-        val iters = Seq(KryoLazyStatsIterator.configure(sft, filter.secondary, hints, deduplicate = false))
-        val kvsToFeatures = KryoLazyStatsIterator.kvsToFeatures(sft)
-        BatchScanPlan(filter, table, ranges, iters, Seq.empty, kvsToFeatures, threads, hasDuplicates = false)
+      if (sft.getSchemaVersion > 5) {
+        // optimized path when we know we're using kryo serialization
+        if (hints.isBinQuery) {
+          // use the server side aggregation
+          val iters = Seq(BinAggregatingIterator.configureDynamic(sft, filter.secondary, hints, dupes))
+          val kvsToFeatures = BinAggregatingIterator.kvsToFeatures()
+          BatchScanPlan(filter, table, ranges, iters, Seq.empty, kvsToFeatures, threads, dupes)
+        } else if (hints.isStatsIteratorQuery) {
+          val iters = Seq(KryoLazyStatsIterator.configure(sft, filter.secondary, hints, dupes))
+          val kvsToFeatures = KryoLazyStatsIterator.kvsToFeatures(sft)
+          BatchScanPlan(filter, table, ranges, iters, Seq.empty, kvsToFeatures, threads, dupes)
+        } else {
+          val iters = KryoLazyFilterTransformIterator.configure(sft, filter.secondary, hints).toSeq
+          val kvsToFeatures = queryPlanner.defaultKVsToFeatures(hints)
+          BatchScanPlan(filter, table, ranges, iters, Seq.empty, kvsToFeatures, threads, dupes)
+        }
       } else {
-        val iters = KryoLazyFilterTransformIterator.configure(sft, filter.secondary, hints).toSeq
-        val kvsToFeatures = queryPlanner.defaultKVsToFeatures(hints)
-        BatchScanPlan(filter, table, ranges, iters, Seq.empty, kvsToFeatures, threads, hasDuplicates = false)
+        val iters = if (filter.secondary.isDefined || hints.getTransformSchema.isDefined) {
+          Seq(configureRecordTableIterator(sft, featureEncoding, filter.secondary, hints))
+        } else {
+          Seq.empty
+        }
+        val kvsToFeatures = if (hints.isBinQuery) {
+          BinAggregatingIterator.nonAggregatedKvsToFeatures(sft, hints, featureEncoding)
+        } else {
+          queryPlanner.defaultKVsToFeatures(hints)
+        }
+        BatchScanPlan(filter, table, ranges, iters, Seq.empty, kvsToFeatures, threads, dupes)
       }
-    } else {
-      val iters = if (filter.secondary.isDefined || hints.getTransformSchema.isDefined) {
-        Seq(configureRecordTableIterator(sft, featureEncoding, filter.secondary, hints))
-      } else {
-        Seq.empty
-      }
-      val kvsToFeatures = if (hints.isBinQuery) {
-        BinAggregatingIterator.nonAggregatedKvsToFeatures(sft, hints, featureEncoding)
-      } else {
-        queryPlanner.defaultKVsToFeatures(hints)
-      }
-      BatchScanPlan(filter, table, ranges, iters, Seq.empty, kvsToFeatures, threads, hasDuplicates = false)
     }
   }
 }
