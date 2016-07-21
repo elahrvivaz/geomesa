6e87caa485e7d0cba34a6a5d177fe0a9e4b66579
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2016-07-21 11:43:15 -0400

    fixed then broke

--- geomesa-z3/src/main/scala/org/locationtech/geomesa/curve/XZ2SFC.scala
+++ geomesa-z3/src/main/scala/org/locationtech/geomesa/curve/XZ2SFC.scala
@@ -8,46 +8,199 @@
 
 package org.locationtech.geomesa.curve
 
-import org.locationtech.geomesa.curve.XZ2SFC.Bounds
+import org.locationtech.geomesa.curve.XZ2SFC.{XElement, QueryWindow}
 import org.locationtech.sfcurve.IndexRange
 
 import scala.collection.mutable.ArrayBuffer
 
+/**
+  * Extended Z-order curve implementation used for efficiently storing polygons.
+  *
+  * Based on 'XZ-Ordering: A Space-Filling Curve for Objects with Spatial Extension'
+  * by Christian Böhm, Gerald Klump  and Hans-Peter Kriegel
+  *
+  * @param g resolution level of the curve - i.e. how many times the space will be recursively quartered
+  */
 class XZ2SFC(g: Short) {
 
-  // TODO see if we can use Ints
-  // require(g < 13, "precision > 12 is not supported by Ints")
+  // TODO see what the max value of g can be where we can use Ints instead of Longs and possibly refactor to use Ints
 
+  /**
+    * Index a polygon by it's bounding box
+    *
+    * @param xmin min x value in [-180,180]
+    * @param ymin min y value in [-90,90]
+    * @param xmax max x value in [-180,180], must be >= xmin
+    * @param ymax max y value in [-90,90], must be >= ymin
+    * @return
+    */
   def index(xmin: Double, ymin: Double, xmax: Double, ymax: Double): Long = {
-    validateBounds(xmin, ymin, xmax, ymax)
-    val bounds = Bounds(normalizeLon(xmin),  normalizeLat(ymin), normalizeLon(xmax), normalizeLat(ymax))
-    index(bounds)
-  }
+    // normalize inputs to [0,1]
+    val (nxmin, nymin, nxmax, nymax) = XZ2SFC.normalize(xmin, ymin, xmax, ymax)
 
-  private def index(bounds: Bounds): Long = {
-    val w = bounds.xmax - bounds.xmin
-    val h = bounds.ymax - bounds.ymin
+    // calculate the length of the sequence code (section 4.1 of XZ-Ordering paper)
 
+    val w = nxmax - nxmin
+    val h = nymax - nymin
+
+    // el-one is a bit confusing to read, but corresponds with the paper's definitions
     val l1 = math.min(g, math.floor(math.log(math.max(w, h)) / math.log(0.5)).toInt)
 
+    // predicate for checking how many axis the polygon intersects
     def predicate(value: Double, wh: Double): Boolean = math.floor((value / l1) + 2) * l1 <= value + wh
 
-    val length = if (predicate(bounds.xmin, w) && predicate(bounds.ymin, h)) l1 else l1 + 1
+    // note: this part of the paper is slightly confusing - do both x AND y have to satisfy the predicate?
+    // from experimental results seems that way
+    val length = if (predicate(nxmin, w) && predicate(nymin, h)) l1 else l1 + 1
 
-    sequenceCode(bounds.xmin, bounds.ymin, length)
+    sequenceCode(nxmin, nymin, length)
   }
 
-  // normalize to [0, 1]
-  private def normalizeLon(x: Double): Double = (x + 180.0) / 360.0
-  private def normalizeLat(y: Double): Double = (y + 90.0) / 180.0
-
-  private def validateBounds(xmin: Double, ymin: Double, xmax: Double, ymax: Double): Unit = {
-    require(xmin <= 180.0 && xmin >= -180.00 && xmax <= 180.0 && xmax >= -180.00 &&
-        ymin <= 90.0 && ymin >= -90.00 && ymax <= 90.0 && ymax >= -90.00,
-      s"Bounds must be within [-180 180] [-90 90]: [$xmin $xmax] [$ymin $ymax]"
-    )
+  /**
+    * Determine XZ-curve ranges that will cover a given query window
+    *
+    * @param queries a sequence of OR'd windows to cover. Each window is in the form (xmin, ymin, xmax, ymax) where:
+    *                  xmin, xmax are both in [-180,180]
+    *                  ymin, ymax are both in [-90,90]
+    *                  xmax >= xmin
+    *                  ymax >= ymin
+    * @param maxRanges a rough upper limit on the number of ranges to generate
+    * @return
+    */
+  def ranges(queries: Seq[(Double, Double, Double, Double)], maxRanges: Option[Int] = None): Seq[IndexRange] = {
+    // normalize inputs to [0,1]
+    val windows = queries.map { case (xmin, ymin, xmax, ymax) =>
+      val (nxmin, nymin, nxmax, nymax) = XZ2SFC.normalize(xmin, ymin, xmax, ymax)
+      QueryWindow(nxmin, nymin, nxmax, nymax)
+    }
+    ranges(windows.toArray, maxRanges.getOrElse(Int.MaxValue))
   }
 
+  /**
+    * Determine XZ-curve ranges that will cover a given query window
+    *
+    * @param query a sequence of OR'd windows to cover, normalized to [0,1]
+    * @param rangeStop a rough max value for the number of ranges to return
+    * @return
+    */
+  private def ranges(query: Array[QueryWindow], rangeStop: Int): Seq[IndexRange] = {
+
+    import XZ2SFC.LevelTerminator
+
+    // stores our results - initial size of 100 in general saves us some re-allocation
+    val ranges = new java.util.ArrayList[IndexRange](100)
+
+    // values remaining to process - initial size of 100 in general saves us some re-allocation
+    val remaining = new java.util.ArrayDeque[XElement](100)
+
+    // checks if a quad is contained in the search space
+    def isContained(quad: XElement): Boolean = {
+      var i = 0
+      while (i < query.length) {
+        if (quad.isContained(query(i))) {
+          return true
+        }
+        i += 1
+      }
+      false
+    }
+
+    // checks if a quad overlaps the search space
+    def isOverlapped(quad: XElement): Boolean = {
+      var i = 0
+      while (i < query.length) {
+        if (quad.overlaps(query(i))) {
+          return true
+        }
+        i += 1
+      }
+      false
+    }
+
+    // checks a single value and either:
+    //   eliminates it as out of bounds
+    //   adds it to our results as fully matching, or
+    //   adds it to our results as partial matching and queues up it's children for further processing
+    def checkValue(quad: XElement, level: Short): Unit = {
+      if (isContained(quad)) {
+        // whole range matches, happy day
+        val (min, max) = sequenceInterval(quad.xmin, quad.ymin, level, partial = false)
+        ranges.add(IndexRange(min, max, contained = true))
+      } else if (isOverlapped(quad)) {
+        // some portion of this range is excluded
+        // add the partial match and queue up each sub-range for processing
+        val (min, max) = sequenceInterval(quad.xmin, quad.ymin, level, partial = true)
+        ranges.add(IndexRange(min, max, contained = false))
+        quad.children.foreach(remaining.add)
+      }
+    }
+
+    // initial level
+    XElement(0.0, 0.0, 1.0, 1.0).children.foreach(remaining.add)
+    remaining.add(LevelTerminator)
+
+    // level of recursion
+    var level: Short = 1
+
+    while (level <= g && !remaining.isEmpty && ranges.size < rangeStop) {
+      val next = remaining.poll
+      if (next.eq(LevelTerminator)) {
+        // we've fully processed a level, increment our state
+        if (!remaining.isEmpty) {
+          level = (level + 1).toShort
+          remaining.add(LevelTerminator)
+        }
+      } else {
+        checkValue(next, level)
+      }
+    }
+
+    // bottom out and get all the ranges that partially overlapped but we didn't fully process
+    while (!remaining.isEmpty) {
+      val quad = remaining.poll
+      if (quad.eq(LevelTerminator)) {
+        level = (level + 1).toShort
+      } else {
+        val (min, max) = sequenceInterval(quad.xmin, quad.ymin, level, partial = false)
+        ranges.add(IndexRange(min, max, contained = false))
+      }
+    }
+
+    // we've got all our ranges - now reduce them down by merging overlapping values
+    // note: we don't bother reducing the ranges as in the XZ paper, as accumulo handles lots of ranges fairly well
+    ranges.sort(IndexRange.IndexRangeIsOrdered)
+
+    var current = ranges.get(0) // note: should always be at least one range
+    val result = ArrayBuffer.empty[IndexRange]
+    var i = 1
+    while (i < ranges.size()) {
+      val range = ranges.get(i)
+      if (range.lower <= current.upper + 1) {
+        // merge the two ranges
+        current = IndexRange(current.lower, math.max(current.upper, range.upper), current.contained && range.contained)
+      } else {
+        // append the last range and set the current range for future merging
+        result.append(current)
+        current = range
+      }
+      i += 1
+    }
+    // append the last range - there will always be one left that wasn't added
+    result.append(current)
+
+    result
+  }
+
+  /**
+    * Computes the sequence code for a given point - for polygons this is the lower-left corner.
+    *
+    * Based on Definition 2 from the XZ-Ordering paper
+    *
+    * @param x normalized x value [0,1]
+    * @param y normalized y value [0,1]
+    * @param length length of the sequence code that will be generated
+    * @return
+    */
   private def sequenceCode(x: Double, y: Double, length: Int): Long = {
     var xmin = 0.0
     var ymin = 0.0
@@ -73,149 +226,99 @@ class XZ2SFC(g: Short) {
     cs
   }
 
-  def ranges(bounds: Seq[(Double, Double, Double, Double)],
-             maxRanges: Option[Int] = None,
-             maxRecurse: Option[Int] = Some(XZ2SFC.DefaultRecurse)): Seq[IndexRange] = {
-    val normalizedBounds = bounds.map { case (xmin, ymin, xmax, ymax) =>
-      validateBounds(xmin, ymin, xmax, ymax)
-      Bounds(normalizeLon(xmin),  normalizeLat(ymin), normalizeLon(xmax),normalizeLat(ymax))
+  /**
+    * Computes an interval of sequence codes for a given point - for polygons this is the lower-left corner.
+    *
+    * @param x normalized x value [0,1]
+    * @param y normalized y value [0,1]
+    * @param length length of the sequence code that will used as the basis for this interval
+    * @param partial true if the element partially intersects the query window, false if it is fully contained
+    * @return
+    */
+  private def sequenceInterval(x: Double, y: Double, length: Short, partial: Boolean): (Long, Long) = {
+    val min = sequenceCode(x, y, length)
+    // if a partial match, we just use the single sequence code as an interval
+    // if a full match, we have to match all sequence codes starting with the single sequence code
+    val max = if (partial) { min } else {
+      // from lemma 3 in the XZ-Ordering paper
+      min + (math.pow(4, g - length + 1).toLong - 1L) / 3L
     }
-    val rangeStop = maxRanges.getOrElse(Int.MaxValue)
-    val recurseStop = math.min(maxRecurse.getOrElse(XZ2SFC.DefaultRecurse), g)
-    ranges(normalizedBounds, rangeStop, recurseStop)
-  }
-
-  private def ranges(bounds: Seq[Bounds], rangeStop: Int, recurseStop: Int): Seq[IndexRange] = {
-
-    import XZ2SFC.LevelTerminator
-
-    // stores our results - initial size of 100 in general saves us some re-allocation
-    val ranges = new java.util.ArrayList[IndexRange](100)
-
-    // values remaining to process - initial size of 100 in general saves us some re-allocation
-    val remaining = new java.util.ArrayDeque[Bounds](100)
-
-    // checks if a range is contained in the search space
-    def isContained(quad: Bounds): Boolean = {
-      var i = 0
-      while (i < bounds.length) {
-        if (bounds(i).contains(quad)) {
-          return true
-        }
-        i += 1
-      }
-      false
-    }
-
-    // checks if a range overlaps the search space
-    def isOverlapped(quad: Bounds): Boolean = {
-      var i = 0
-      while (i < bounds.length) {
-        if (bounds(i).overlaps(quad)) {
-          return true
-        }
-        i += 1
-      }
-      false
-    }
-
-    // checks a single value and either:
-    //   eliminates it as out of bounds
-    //   adds it to our results as fully matching, or
-    //   queues up it's children for further processing
-    def checkValue(quad: Bounds, level: Short): Unit = {
-      if (isContained(quad)) {
-        // whole range matches, happy day
-        val (min, max) = sequenceInterval(quad.xmin, quad.ymin, level, partial = false)
-        ranges.add(IndexRange(min, max, contained = true))
-      } else if (isOverlapped(quad)) {
-        // some portion of this range is excluded
-        // add the partial match and queue up each sub-range for processing
-        val (min, max) = sequenceInterval(quad.xmin, quad.ymin, level, partial = true)
-        ranges.add(IndexRange(min, max, contained = false))
-        quad.children.foreach(remaining.add)
-      }
-    }
-
-    def sequenceInterval(x: Double, y: Double, level: Short, partial: Boolean): (Long, Long) = {
-      val min = sequenceCode(x, y, level)
-      // there should be this many values starting with this prefix: (math.pow(4, g - level + 1).toLong - 1L) / 3L
-      // so take the minimum sequnce code and add this?
-      val max = if (partial) { min } else { min + (math.pow(4, g - level + 1).toLong - 1L) / 3L }
-      (min, max)
-    }
-
-    // initial level
-    Bounds(0.0, 0.0, 1.0, 1.0).children.foreach(remaining.add)
-    remaining.add(LevelTerminator)
-
-    // level of recursion
-    var level: Short = 1
-
-    while (level <= recurseStop && !remaining.isEmpty && ranges.size < rangeStop) {
-      val next = remaining.poll
-      if (next.eq(LevelTerminator)) {
-        // we've fully processed a level, increment our state
-        if (!remaining.isEmpty) {
-          level = (level + 1).toShort
-          remaining.add(LevelTerminator)
-        }
-      } else {
-        checkValue(next, level)
-      }
-    }
-
-    // bottom out and get all the ranges that partially overlapped but we didn't fully process
-    while (!remaining.isEmpty) {
-      val quad = remaining.poll
-      if (quad.eq(LevelTerminator)) {
-        level = (level + 1).toShort
-      } else {
-        val (min, max) = sequenceInterval(quad.xmin, quad.ymin, level, partial = false)
-        ranges.add(IndexRange(min, max, contained = false))
-      }
-    }
-
-    // TODO we could use the algorithm from the XZ paper instead
-    // we've got all our ranges - now reduce them down by merging overlapping values
-    ranges.sort(IndexRange.IndexRangeIsOrdered)
-
-    var current = ranges.get(0) // note: should always be at least one range
-    val result = ArrayBuffer.empty[IndexRange]
-    var i = 1
-    while (i < ranges.size()) {
-      val range = ranges.get(i)
-      if (range.lower <= current.upper + 1) {
-        // merge the two ranges
-        current = IndexRange(current.lower, math.max(current.upper, range.upper), current.contained && range.contained)
-      } else {
-        // append the last range and set the current range for future merging
-        result.append(current)
-        current = range
-      }
-      i += 1
-    }
-    // append the last range - there will always be one left that wasn't added
-    result.append(current)
-
-    result
+    (min, max)
   }
 }
 
 object XZ2SFC {
 
-  val DefaultRecurse = 10
+  val QueryMinX = -180.0
+  val QueryMaxX = 180.0
+  val QueryMinY = -90.0
+  val QueryMaxY = 90.0
+
+  private val QueryRangeX = QueryMaxX - QueryMinX
+  private val QueryRangeY = QueryMaxY - QueryMinY
+
+  /**
+    * Normalize lat/lon to [0,1]
+    *
+    * @param xmin min x value in [-180,180]
+    * @param ymin min y value in [-90,90]
+    * @param xmax max x value in [-180,180], must be >= xmin
+    * @param ymax max y value in [-90,90], must be >= ymin
+    * @return
+    */
+  def normalize(xmin: Double, ymin: Double, xmax: Double, ymax: Double): (Double, Double, Double, Double) = {
+    require(xmin <= QueryMaxX && xmin >= QueryMinX && xmax <= QueryMaxX && xmax >= QueryMinX &&
+        ymin <= QueryMaxY && ymin >= QueryMinY && ymax <= QueryMaxY && ymax >= QueryMinY,
+      s"Bounds must be within [$QueryMinX $QueryMaxX] [$QueryMinY $QueryMaxY]: [$xmin $xmax] [$ymin $ymax]"
+    )
+    require(xmin <= xmax && ymin <= ymax, s"Bounds must be ordered: [$xmin $xmax] [$ymin $ymax]")
+    val nxmin = (xmin + QueryMaxX) / QueryRangeX
+    val nymin = (ymin + QueryMaxY) / QueryRangeY
+    val nxmax = (xmax + QueryMaxX) / QueryRangeX
+    val nymax = (ymax + QueryMaxY) / QueryRangeY
+    (nxmin, nymin, nxmax, nymax)
+  }
 
   // indicator that we have searched a full level of the quad/oct tree
-  private val LevelTerminator = Bounds(-1.0, -1.0, -1.0, -1.0)
+  private val LevelTerminator = XElement(-1.0, -1.0, -1.0, -1.0)
 
-  private case class Bounds(xmin: Double, ymin: Double, xmax: Double, ymax: Double) {
+  /**
+    * Region being queried. Bounds are normalized to [0-1].
+    *
+    * @param xmin x lower bound in [0-1]
+    * @param ymin y lower bound in [0-1]
+    * @param xmax x upper bound in [0-1], must be >= xmin
+    * @param ymax y upper bound in [0-1], must be >= ymin
+    */
+  private case class QueryWindow(xmin: Double, ymin: Double, xmax: Double, ymax: Double)
 
-    def contains(value: Bounds) = xmin <= value.xmin && xmax >= value.xmax && ymin <= value.ymin && ymax >= value.ymax
+  /**
+    * An extended Z curve element. Bounds refer to the non-extended z element for simplicity of calculation.
+    *
+    * An extended Z element refers to a normal Z curve element that has it's upper bounds expanded by double it's
+    * width/height. By convention, an element is always square.
+    *
+    * @param xmin x lower bound in [0-1]
+    * @param ymin y lower bound in [0-1]
+    * @param xmax x upper bound in [0-1], must be >= xmin
+    * @param ymax y upper bound in [0-1], must be >= ymin
+    */
+  private case class XElement(xmin: Double, ymin: Double, xmax: Double, ymax: Double) {
 
-    def overlaps(value: Bounds) = xmin <= value.xmax && xmax >= value.xmin && ymin <= value.ymax && ymax >= value.ymin
+    // length of the non-extended side (note: by convention width should be equal to height)
+    lazy private val length = xmax - xmin
 
-    def children: Seq[Bounds] = {
+    // extended x and y bounds
+    lazy val xext = xmax + length
+    lazy val yext = ymax + length
+
+    def isContained(window: QueryWindow): Boolean =
+      window.xmin <= xmin && window.ymin <= ymin && window.xmax >= xext && window.ymax >= yext
+
+    def overlaps(window: QueryWindow): Boolean =
+      window.xmax >= xmin && window.ymax >= ymin && window.xmin <= xext && window.ymin <= yext
+
+    def children: Seq[XElement] = {
       val xCenter = (xmin + xmax) / 2.0
       val yCenter = (ymin + ymax) / 2.0
       val c0 = copy(xmax = xCenter, ymax = yCenter)
--- geomesa-z3/src/test/scala/org/locationtech/geomesa/curve/XZ2SFCTest.scala
+++ geomesa-z3/src/test/scala/org/locationtech/geomesa/curve/XZ2SFCTest.scala
@@ -15,10 +15,7 @@ import org.specs2.runner.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class XZ2SFCTest extends Specification {
 
-  val sfc = new XZ2SFC(12)
-
-
-  type MyTuple = (Long, Long)
+  val sfc = new XZ2SFC(6)
 
   "XZ2" should {
     "index polygons and query them" >> {
@@ -39,9 +36,9 @@ class XZ2SFCTest extends Specification {
       val disjoint = Seq(
         (-180.0, -90.0, 8.0, 8.0),
         (0.0, 0.0, 8.0, 8.0),
-        (9.0, 9.0, 9.5, 9.5),
-        (12.5, 12.5, 13.5, 13.5),
-        (20.0, 20.0, 180.0, 90.0)
+//        (9.0, 9.0, 9.5, 9.5),
+        (12.5, 12.5, 13.5, 13.5)
+//        (20.0, 20.0, 180.0, 90.0)
       )
       forall(containing ++ overlapping) { bbox =>
         val ranges = sfc.ranges(Seq(bbox)).map(r => (r.lower, r.upper))
@@ -76,12 +73,13 @@ class XZ2SFCTest extends Specification {
         (10.5, 10.5, 11.5, 11.5),
         (11.0, 11.0, 11.0, 11.0)
       )
+      // note: not all disjoint ranges will
       val disjoint = Seq(
         (-180.0, -90.0, 8.0, 8.0),
         (0.0, 0.0, 8.0, 8.0),
-        (9.0, 9.0, 9.5, 9.5),
-        (12.5, 12.5, 13.5, 13.5),
-        (20.0, 20.0, 180.0, 90.0)
+//        (9.0, 9.0, 9.5, 9.5),
+        (12.5, 12.5, 13.5, 13.5)
+//        (20.0, 20.0, 180.0, 90.0)
       )
       forall(containing ++ overlapping) { bbox =>
         val ranges = sfc.ranges(Seq(bbox)).map(r => (r.lower, r.upper))
@@ -100,5 +98,55 @@ class XZ2SFCTest extends Specification {
         matches must beFalse
       }
     }
+
+    "index complex features and query them2" >> {
+      val polys = Seq(
+        (43.85189184740719,22.40536415671486,47.30967787376657,29.84841540199809),
+        (43.85189184740719,22.40536415671486,47.30967787376657,29.84841540199809),
+        (41.432203853005966,21.339398405868923,49.7857434014784,27.22357119188849),
+        (41.432203853005966,21.339398405868923,49.7857434014784,27.22357119188849),
+        (40.71498314879894,21.789879845288173,48.59356551354648,29.715469888517127),
+        (40.71498314879894,21.789879845288173,48.59356551354648,29.715469888517127),
+        (47.713129661706795,20.09673497300974,48.54487167042291,29.45333238959629),
+        (47.713129661706795,20.09673497300974,48.54487167042291,29.45333238959629),
+        (40.716236234178005,20.29107512721636,49.719501209537455,29.891171507514056),
+        (40.716236234178005,20.29107512721636,49.719501209537455,29.891171507514056),
+        (41.67389088584407,20.03913530617221,48.92816957156185,23.890431835536287)
+      )
+
+      val ranges = sfc.ranges(Seq((45.0, 23.0, 48.0, 27.0)))
+      forall(polys) { p =>
+        val index = sfc.index(p._1, p._2, p._3, p._4)
+        val matches = ranges.exists(r => r.lower <= index && r.upper >= index)
+        if (!matches) {
+          println(s"$p - invalid match")
+        }
+        matches must beTrue
+      }
+    }
+
+//    "index complex features and query them" >> {
+//      val rPoint = """(\d+\.?\d*) (\d+\.?\d*)""".r
+//      val file = Source.fromURL(getClass.getClassLoader.getResource("geoms.list"))
+//      val geoms = file.getLines().toList.flatMap {
+//        case line if line.startsWith("POINT") =>
+//         val points = rPoint.findFirstMatchIn(line).map(m => (m.group(1).toDouble, m.group(2).toDouble))
+//          points.map(p => (p._1, p._2, p._1, p._2))
+//        case line if line.startsWith("LINESTRING") =>
+//          val points = rPoint.findAllMatchIn(line).map(m => (m.group(1).toDouble, m.group(2).toDouble)).toSeq
+//          Some((points.map(_._1).min, points.map(_._2).min, points.map(_._1).max, points.map(_._2).max))
+//        case line if line.startsWith("POLYGON") =>
+//          val points = rPoint.findAllMatchIn(line).map(m => (m.group(1).toDouble, m.group(2).toDouble)).toSeq
+//          Some((points.map(_._1).min, points.map(_._2).min, points.map(_._1).max, points.map(_._2).max))
+//      }
+//      file.close()
+//
+//      val indexedGeoms = geoms.map { case (xmin, ymin, xmax, ymax) => sfc.index(xmin, ymin, xmax, ymax) }
+//      val ranges = sfc.ranges(Seq((45.0, 23.0, 48.0, 27.0)))
+////      geoms.filter { case (xmin, ymin, xmax, ymax) => val g = sfc.index(xmin, ymin, xmax, ymax); !ranges.exists(r => r.lower <= g && r.upper >= g) }.foreach(println)
+//      forall(indexedGeoms) { g =>
+//        ranges.exists(r => r.lower <= g && r.upper >= g) must beTrue
+//      }
+//    }
   }
 }
