54ab7fad0c14638803d4977614922aa33939fd11
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2020-02-28 17:09:30 -0500

    GEOMESA-2638 HBase 2.2 support
    * New artifacts for use with HBase 2:
      * geomesa-hbase-distributed-runtime-hbase2_2.11
      * geomesa-hbase-spark-runtime-hbase2_2.11
    * Update default HBase version to 2.2.3
    * Add Maven profile `hbase1` for testing against HBase 1.4
    * Replace HBaseTestRunnerTest with singleton MiniCluster instance
    
    Signed-off-by: Emilio Lahr-Vivaz <elahrvivaz@ccri.com>

---
+++ geomesa-hbase/geomesa-hbase-server/src/main/scala/org/locationtech/geomesa/hbase/server/common/HBaseVersionAggregator.scala
@@ -0,0 +1,56 @@
+/***********************************************************************
+ * Copyright (c) 2013-2020 Commonwealth Computer Research, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution and is available at
+ * http://www.opensource.org/licenses/apache2.0.php.
+ ***********************************************************************/
+
+package org.locationtech.geomesa.hbase.server.common
+
+import java.nio.charset.StandardCharsets
+
+import org.apache.hadoop.hbase.regionserver.RegionScanner
+import org.locationtech.geomesa.hbase.server.common.HBaseVersionAggregator.VersionAggregator
+import org.locationtech.geomesa.index.iterators.AggregatingScan.AggregateCallback
+import org.locationtech.geomesa.utils.conf.GeoMesaProperties
+import org.opengis.feature.simple.{SimpleFeature, SimpleFeatureType}
+
+class HBaseVersionAggregator extends HBaseAggregator[VersionAggregator] {
+
+  private var scanned = false
+
+  override def setScanner(scanner: RegionScanner): Unit = {}
+
+  override def init(options: Map[String, String]): Unit = {}
+
+  override def aggregate[A <: AggregateCallback](callback: A): A = {
+    if (!scanned) {
+      scanned = true
+      callback.batch(GeoMesaProperties.ProjectVersion.getBytes(StandardCharsets.UTF_8))
+    }
+    callback
+  }
+
+  override protected def initResult(
+      sft: SimpleFeatureType,
+      transform: Option[SimpleFeatureType],
+      batchSize: Int,
+      options: Map[String, String]): VersionAggregator = throw new NotImplementedError()
+
+  override protected def defaultBatchSize: Int = throw new NotImplementedError()
+
+  override protected def aggregateResult(sf: SimpleFeature, result: VersionAggregator): Int =
+    throw new NotImplementedError()
+
+  override protected def encodeResult(result: VersionAggregator): Array[Byte] = throw new NotImplementedError()
+
+  override protected def closeResult(result: VersionAggregator): Unit = {}
+}
+
+object HBaseVersionAggregator {
+  class VersionAggregator {
+    def isEmpty: Boolean = false
+    def clear(): Unit = {}
+  }
+}
