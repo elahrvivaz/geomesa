d90f52ccbb3f7ddd471d9b5f4d308ba1228c964a
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2022-09-14 16:25:48 -0400

    fix licenses

--- geomesa-z3/src/main/scala/org/locationtech/geomesa/zorder/sfcurve/package.scala
+++ geomesa-z3/src/main/scala/org/locationtech/geomesa/zorder/sfcurve/package.scala
@@ -1,11 +1,11 @@
-/** *********************************************************************
+/***********************************************************************
  * Copyright (c) 2013-2022 Commonwealth Computer Research, Inc.
  * Copyright (c) 2015 Azavea.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License, Version 2.0
  * which accompanies this distribution and is available at
  * http://www.opensource.org/licenses/apache2.0.php.
- * ********************************************************************* */
+ ***********************************************************************/
 
 package org.locationtech.geomesa.zorder
 
